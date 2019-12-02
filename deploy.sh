304fc9eb51b38e64da16ffb4f8ec1926a8dbed41
Vital Fadeev <vital.fadeev@gmail.com>
2019-12-02 16:58:46 +0700

    * fixes

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,3 @@
 #!/bin/bash
 
-_message() {
-    MESSAGE=$*
-    echo -e "\e[32m${MESSAGE}\e[0m"
-}
-
-# cd site
-cd /home/www/htdocs/AI/
-
-
-# Getting fresh sources
-# Note: deploy was created by command: git remote add deploy ssh://git@github.com/vitalfadeev/AI.git
-_message "Getting fresh sources"
-git pull deploy master
-
-
-# Reloading Apache
-_message "Reloading Apache"
-systemctl reload apache2
-
-
-# All done!
-_message "All done!"
-
-
-# it runned like a: ssh root@94.23.192.197 /home/www/htdocs/AI/deploy.sh
-#
-# or
-# Login via ssh
-# ssh root@94.23.192.197
-# /home/www/htdocs/AI/deploy.sh
+ssh root@94.23.192.197 /home/www/htdocs/AI/deploy.sh
